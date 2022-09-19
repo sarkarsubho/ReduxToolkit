f49777252b2acd8d47b15b5d411530a8a7ec2ace
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-09-19 23:45:36 +0530

    commit

---
+++ redux_toolkit/src/App.jsx
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Cart } from './pages/Cart';
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path="/cart" element={<Cart></Cart>}></Route>
+      </Routes>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ redux_toolkit/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Navbar = () => {
+  return <div>
+
+    <span> Redux Toolkit</span>
+    <div>
+        
+    </div>
+  </div>;
+};
