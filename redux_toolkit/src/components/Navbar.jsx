84f87efc2329511ad764585f11a51811d6e194df
sarkarsubho <93681750+sarkarsubho@users.noreply.github.com>
2022-09-22 08:47:23 +0530

    commit

--- redux_toolkit/src/components/Navbar.jsx
+++ redux_toolkit/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
   const items =useSelector(state=>state.cart)
   return <div className="nav">
 
-    <span className="logo"> Redux Toolkit</span>
+    <span className="logo"> Redux Toolkit Store</span>
     <div>
       <Link  className="navlink" to="/">Home</Link>
       <Link className="navlink" to="/cart">Cart</Link>
